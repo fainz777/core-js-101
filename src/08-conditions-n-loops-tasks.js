951269b2a901ae4a9e94aade8080c7baecb5fcc0
Oksana Fainitskaia <of@devinform.ru>
2020-03-03 19:25:24 +0300

    feat: task solution

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -27,8 +27,18 @@
  *  21 => 'Fizz'
  *
  */
-function getFizzBuzz(/* num */) {
-  throw new Error('Not implemented');
+function getFizzBuzz(num) {
+  let value = num;
+
+  if (num % 15 === 0) {
+    value = 'FizzBuzz';
+  } else if (num % 5 === 0) {
+    value = 'Buzz';
+  } else if (num % 3 === 0) {
+    value = 'Fizz';
+  }
+
+  return value;
 }
 
 
@@ -43,8 +53,14 @@ function getFizzBuzz(/* num */) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  let fact = 1;
+
+  for (let i = 2; i < n; i += 1) {
+    fact *= i;
+  }
+
+  return fact;
 }
 
 
@@ -60,8 +76,14 @@ function getFactorial(/* n */) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2) {
+  let sum = n1;
+
+  for (let i = n1 + 1; i < n2; i += 1) {
+    sum += i;
+  }
+
+  return sum;
 }
 
 
